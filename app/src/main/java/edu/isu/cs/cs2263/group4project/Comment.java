b958fd2f102dc1a3354d9b75e901464ac6316bcf
Pawan <achapawa@isu.edu>
2021-03-21 15:30:54 -0600

    Added comment class

---
+++ app/src/main/java/edu/isu/cs/cs2263/group4project/Comment.java
@@ -0,0 +1,19 @@
+package edu.isu.cs.cs2263.group4project;
+
+
+public class Comment {
+    private String comment;
+
+    /*Constructor */
+    public Comment(String comment){
+        this.comment=comment;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
