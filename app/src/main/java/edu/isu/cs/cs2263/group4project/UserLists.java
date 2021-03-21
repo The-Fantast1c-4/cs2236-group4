f689813df285f74216d0175a0fcc066f6f199827
Pawan <achapawa@isu.edu>
2021-03-21 00:23:13 -0600

    UserLists added

---
+++ app/src/main/java/edu/isu/cs/cs2263/group4project/UserLists.java
@@ -0,0 +1,53 @@
+package edu.isu.cs.cs2263.group4project;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserLists {
+    /* Arraylist to store List   */
+    private ArrayList<List> lists;
+    /* text obtained from User  */
+    private String searchText;
+
+    /*Constructor */
+    public UserLists(){
+
+    }
+
+    /* Methods */
+    /*This method searches the text and returns the matched object as a list. */
+    public List<Object> searchItems(String searchText){
+
+
+        /*returns a list of objects */
+    }
+
+    /* This method helps to make a new list*/
+    public void makeList(String name,String description){
+
+        //Also saves it as  a data
+    }
+
+    /* Method to return lists */
+    public List<List> getLists(){
+        return lists;
+        //returns lists
+    }
+
+    /* Method to return Archived List */
+    public List<List> getArchivedLists(){
+
+    }
+
+    /* Method to return Non Archived List */
+    public List<List> getNonArchivedLists(){
+
+    }
+
+    
+
+
+
+
+
+}
