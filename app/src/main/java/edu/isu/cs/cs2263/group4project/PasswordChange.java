a98c1c1ad5d654973ab1442d1ca35a392175f03f
Pawan <achapawa@isu.edu>
2021-03-27 23:04:03 -0600

    Added Password Change state

---
+++ app/src/main/java/edu/isu/cs/cs2263/group4project/PasswordChange.java
@@ -0,0 +1,76 @@
+package edu.isu.cs.cs2263.group4project;
+
+
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class PasswordChange  {
+    public PasswordChange() {
+    }
+    //main method
+    public static void main(String[] args) {
+        Application.launch(args);
+    }
+
+    public void handle(Object event) {
+    }
+
+    public void run() {
+    }
+
+    public void start(Stage primaryStage) throws Exception {
+        this.testState(primaryStage);
+    }
+
+    public void testState(Stage stage) {
+        //creating child nodes
+        stage.setTitle("Password Change");
+        Button changePassword = new Button("Change Password");
+        Button exit = new Button("Exit   ");
+        TextField userFirstLast = new TextField();
+        userFirstLast.setPromptText("FirstName LastName");
+        PasswordField password = new PasswordField();
+        PasswordField verification = new PasswordField();
+        PasswordField adminPassword = new PasswordField();
+        Label newPassword= new Label("New Password");
+        Label verifyPassword = new Label("Verify Password");
+        Label adminPass = new Label("Admin Password");
+
+        //creating Gridpane
+        GridPane gridPane = new GridPane();
+        gridPane.setPadding(new Insets(20.0D, 20.0D, 20.0D, 20.0D));
+        gridPane.setVgap(30.0D);
+        gridPane.setHgap(20.0D);
+        gridPane.setAlignment(Pos.CENTER);
+
+        //adding nodes to gridPane
+        gridPane.add(userFirstLast, 0, 0);
+        gridPane.add(newPassword, 0, 1);
+        gridPane.add(password, 1, 1);
+        gridPane.add(verifyPassword, 0, 2);
+        gridPane.add(verification, 1, 2);
+        gridPane.add(adminPass, 0, 3);
+        gridPane.add(adminPassword, 1, 3);
+        gridPane.add(changePassword,0,4);
+        gridPane.add(exit,1,4);
+
+        //color of bittons and background
+        changePassword.setStyle("-fx-background-color: #e48257;");
+        exit.setStyle("-fx-background-color: #e48257;");
+        gridPane.setStyle("-fx-background-color: #f2edd7;");
+
+        //adding layout to the scene
+        Scene scene = new Scene(gridPane);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
